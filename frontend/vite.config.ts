206c068ae34fa5d230d372f72c39e415149f6bab
leila <l.talebzadeh93@gmail.com>
2026-06-29 15:48:01 +0330

    build: add @sveltejs/adapter-cloudflare

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
